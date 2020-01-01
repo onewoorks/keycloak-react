5c7d5c828d3c59af8212b8b44ec09a3670c41745
onewoorks <irwanbibrahim@onewoorks.com>
2020-01-01 11:54:35 +0800

    simulate sso dua port

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import Welcome from './Welcome';
 import Secured from './Secured';
+import Secured2 from './Secured2';
 import './App.css';
 
 class App extends Component {
@@ -12,10 +13,12 @@ class App extends Component {
         <div className="container">
           <ul>
             <li><Link to="/">public component</Link></li>
-            <li><Link to="/secured">secured component</Link></li>
+            <li><Link to="/secured">secured component 1 untuk 3000</Link></li>
+            <li><Link to="/secured2">secured component 2 untuk 3001</Link></li>
           </ul>
           <Route exact path="/" component={Welcome} />
           <Route path="/secured" component={Secured} />
+          <Route path="/secured2" component={Secured2} />
         </div>
       </BrowserRouter>
     );
